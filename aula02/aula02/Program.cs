9e3b3821680192c913ab1d484c3809f50665e847
Lucas Rockenbach <105431330+LucasRockenbach@users.noreply.github.com>
2023-08-10 21:06:25 -0300

    Add files via upload

---
+++ aula02/aula02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminhoArquivo = "C:/Users/autologon.CSED/Desktop/aula1Dados.CSV";
+            if (File.Exists(caminhoArquivo))
+            {
+                Stream file = File.Open(caminhoArquivo, FileMode.Open);
+                StreamReader leitor = new StreamReader(file);
+                string linha = leitor.ReadLine();
+                while (linha != null)
+                {
+                    Console.WriteLine(linha + "\n");
+                    linha = leitor.ReadLine();
+                }
+                leitor.Close();
+                file.Close();
+            }
+            else
+            {
+                Console.WriteLine("Nenhum arquivo encontrado");
+            }
+            Console.ReadLine();
+        }
+    }
+}
